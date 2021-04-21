5f9986604f6544bb1a0463251a17b9ac0d8b7cae
musa2018 <musa_hamd1@hotmail.com>
2021-04-21 18:15:50 +0300

    Add Get Book View

--- Webgentle.BookStore/Webgentle.BookStore/Models/BookModel.cs
+++ Webgentle.BookStore/Webgentle.BookStore/Models/BookModel.cs
@@ -11,6 +11,8 @@ namespace Webgentle.BookStore.Models
         public string Title { get; set; }
         public string Author { get; set; }
         public string Description { get; set; }
-
+        public string Category { get; set; }
+        public string Language { get; set; }
+        public int ToTalPages { get; set; }
     }
 }
--- Webgentle.BookStore/Webgentle.BookStore/Repository/BookRepository.cs
+++ Webgentle.BookStore/Webgentle.BookStore/Repository/BookRepository.cs
@@ -25,11 +25,13 @@ namespace Webgentle.BookStore.Repository
         {
             return new List<BookModel>()
             {
-                new BookModel() {id=1,Title="MVC",Author="Musa",Description="This is description of MVC book"},
-                new BookModel() {id=2,Title="C#",Author="John",Description="This is description of C# book"},
-                new BookModel() {id=3,Title="Java",Author="Ali",Description="This is description of Java book"},
-                new BookModel() {id=4,Title="F#",Author="Mohammad",Description="This is description of F# book"},
-                new BookModel() {id=5,Title="VB",Author="Tamer",Description="This is description of VB book"},
+                new BookModel() {id=1,Title="MVC",Author="Musa",Description="This is description of MVC book",Category="Action",Language="English",ToTalPages=598},
+                new BookModel() {id=2,Title="C#",Author="John",Description="This is description of C# book",Category="programming",Language="Arabic",ToTalPages=600},
+                new BookModel() {id=3,Title="Java",Author="Ali",Description="This is description of Java book",Category="Tets",Language="English",ToTalPages=1500},
+                new BookModel() {id=4,Title="F#",Author="Mohammad",Description="This is description of F# book",Category="any",Language="Arabic",ToTalPages=203},
+                new BookModel() {id=5,Title="VB",Author="Tamer",Description="This is description of VB book",Category="story",Language="English",ToTalPages=800},
+                new BookModel() {id=6,Title="JavaScript",Author="loly",Description="This is description of javaScript book",Category="story",Language="Arabic",ToTalPages=574},
+
             };
         }
     }
