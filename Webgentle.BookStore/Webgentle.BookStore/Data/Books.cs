e4d916051f671cb6dd214a72316b32979f910360
musa2018 <musa_hamd1@hotmail.com>
2021-04-25 04:14:44 +0300

    Add DbContext

---
+++ Webgentle.BookStore/Webgentle.BookStore/Data/Books.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webgentle.BookStore.Data
+{
+    public class Books
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Language { get; set; }
+        public int ToTalPages { get; set; }
+    }
+}
